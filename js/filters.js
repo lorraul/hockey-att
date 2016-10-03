d846b29af08c64cd78b5ea28c590eeb085aab34d
lorraul <lorraul@gmail.com>
2016-10-03 11:35:41 +0300

    column filter fixed

--- js/filters.js
+++ js/filters.js
@@ -3,17 +3,23 @@ angular.module('molApp')
 .filter('teamsColumn', function() {
   return function(array, column) {
     var result = [];
+    var a,b;
     results = array.filter(function(item, index){
-        if (
-            index >= column*Math.round(array.length/3) 
-            && 
-            index <= (column*Math.round(array.length/3))+Math.round(array.length/3)-1
-        ) return true;
+        if (column == 0) {
+            a=0; 
+            b=Math.floor(array.length/3)+Math.ceil((array.length/3)%1)-1;
+        }
+        if (column == 1) {
+            a=Math.floor(array.length/3)+Math.ceil((array.length/3)%1);
+            b=2*Math.floor(array.length/3)+Math.ceil((array.length/3)%1)+Math.round((array.length/3)%1)-1;
+        }
+        if (column == 2) {
+            a=2*Math.floor(array.length/3)+Math.ceil((array.length/3)%1)+Math.round((array.length/3)%1);
+            b=3*Math.floor(array.length/3)+Math.ceil((array.length/3)%1)+Math.round((array.length/3)%1)-1
+        }
+        if (index >= a && index <= b) return true;
         return false;
     });
     return results;
   };
-})
-
-;
-
+});
